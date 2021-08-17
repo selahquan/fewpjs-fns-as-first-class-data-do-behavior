53def599a4de16e02fc5763ed0d054cdc6022a7f
Josh <josh.handon@gmail.com>
2021-08-17 18:32:06 -0400

    commit tree ef366092d4bd788d56f9115ef99682e21a7a625f

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  document.getElementById("greeting").innerText = str;
+}
+
+function greet(t) {
+  const tToStr = t.split(":", 1)
+  const clock = parseInt(tToStr);
+  if (clock > 17) return "Good Evening";
+  if (clock < 12) return "Good Morning";
+  return "Good Afternoon";
+}
+
